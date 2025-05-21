cf1a530964853ae639fac803a6bbb8ed01385a21
ezequiel reyes <110490540+EzeReyes@users.noreply.github.com>
2025-05-21 17:06:29 -0300

    cambios de estilo y se agrego seccion adicionales

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-const moduloSimple = document.querySelector('#modulo-simple');
-const moduloExtraordinario = document.querySelector('#modulo-extra');
+const moduloSimple = document.querySelector('#select-mod-simple');
+const moduloExtraordinario = document.querySelector('#select-mod-ext');
+const moduloAdicional = document.querySelector('#adicional');
 const serviciosSimples = document.querySelector('#servicios-modulo-simple');
 const serviciosExtraordinarios = document.querySelector('#servicios-modulo-extra');
+const serviciosAdicionales = document.querySelector('#servicios-adicional');
 const modSRealizados = document.querySelector('.modS-realizados');
 const modERealizados = document.querySelector('.modE-realizados');
+const adicionalesRealizados = document.querySelector('.adicionales-realizados');
 const total = document.querySelector('.total');
 
 let valorModuloSimple = Number(moduloSimple.value) || 0;
 let valorModuloExtraordinario = Number(moduloExtraordinario.value) || 0;
+let valorAdicional = Number(moduloAdicional.value) || 0;
 let cantidadServiciosSimple = 0;
 let cantidadServiciosExtraordinarios = 0;
+let cantidadServiciosAdicionales = 0;
 
 const multiplicar = (a, b) => a * b;
 const sumar = (a, b) => a + b;
@@ -26,6 +31,14 @@ const actualizarResultadoExtraordinario = () => {
     const total = multiplicar(valorModuloExtraordinario, cantidadServiciosExtraordinarios);
     modERealizados.innerHTML = `
         <p>${cantidadServiciosExtraordinarios} Servicios Extraordinarios realizados, suman $${total}</p>
+    `;
+    return total;
+};
+
+const actualizarResultadoAdicional = () => {
+    const total = multiplicar(valorAdicional, cantidadServiciosAdicionales);
+    adicionalesRealizados.innerHTML = `
+        <p>${cantidadServiciosAdicionales} Adicionales realizados, suman $${total}</p>
     `;
     return total;
 };
@@ -60,6 +73,18 @@ serviciosExtraordinarios.addEventListener('change', (e) => {
     actualizarResultadoExtraordinario();
     actualizarTotal();
 });
+
+moduloAdicional.addEventListener('change', (e) => {
+    valorAdicional = Number(e.target.value) || 0;
+    actualizarResultadoAdicional();
+});
+
+serviciosAdicionales.addEventListener('change', (e) => {
+    cantidadServiciosAdicionales = Number(e.target.value) || 0;
+    actualizarResultadoAdicional();
+});
+
+
 
 // Mostrar los valores iniciales al cargar la página
 actualizarTotal();
